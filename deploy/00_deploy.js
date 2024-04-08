631da18efc54cc066aac13f58f519b4f18686287
Jaxcoder <codenamejason@outlook.com>
2024-04-08 19:17:35 -0400

    test contract factory

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -3,42 +3,46 @@ require("hardhat-deploy-ethers")
 
 const { networkConfig } = require("../helper-hardhat-config")
 
-
 const private_key = network.config.accounts[0]
 const wallet = new ethers.Wallet(private_key, ethers.provider)
 
 module.exports = async ({ deployments }) => {
-    const { deploy } = deployments;
+    const { deploy } = deployments
     console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
     const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
-    
-    //deploy Simplecoin
-    const simpleCoin = await deploy("SimpleCoin", {
+    const contractFactory = await deploy("ContractFactory", {
         from: wallet.address,
-        args: [tokensToBeMinted],
+        args: [],
         log: true,
-    });
+    })
 
-    //deploy FilecoinMarketConsumer
-    const filecoinMarketConsumer = await deploy("FilecoinMarketConsumer", {
-        from: wallet.address,
-        args: [],
-        log: true,
-    });
+    // //deploy Simplecoin
+    // const simpleCoin = await deploy("SimpleCoin", {
+    //     from: wallet.address,
+    //     args: [tokensToBeMinted],
+    //     log: true,
+    // });
 
-    //deploy DealRewarder
-    const dealRewarder = await deploy("DealRewarder", {
-        from: wallet.address,
-        args: [],
-        log: true,
-    });
-    
-    //deploy DealClient
-    const dealClient = await deploy("DealClient", {
-        from: wallet.address,
-        args: [],
-        log: true,
-    });
+    // //deploy FilecoinMarketConsumer
+    // const filecoinMarketConsumer = await deploy("FilecoinMarketConsumer", {
+    //     from: wallet.address,
+    //     args: [],
+    //     log: true,
+    // });
+
+    // //deploy DealRewarder
+    // const dealRewarder = await deploy("DealRewarder", {
+    //     from: wallet.address,
+    //     args: [],
+    //     log: true,
+    // });
+
+    // //deploy DealClient
+    // const dealClient = await deploy("DealClient", {
+    //     from: wallet.address,
+    //     args: [],
+    //     log: true,
+    // });
 }
